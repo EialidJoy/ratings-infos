dcd184f884494c1f9fe79aae6809e7e585e0e168
Eialid Ahmed Joy <eialid@businessaccelerate.com.bd>
2022-04-16 16:30:03 +0600

    updated commit

--- src/main/java/com/eialid/ratingsdataservice/controllers/RatingsInfoService.java
+++ src/main/java/com/eialid/ratingsdataservice/controllers/RatingsInfoService.java
@@ -1,9 +1,13 @@
 package com.eialid.ratingsdataservice.controllers;
 
 import com.eialid.ratingsdataservice.models.Rating;
+import com.eialid.ratingsdataservice.models.UserRatings;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratings")
@@ -12,5 +16,17 @@ public class RatingsInfoService {
     @RequestMapping("/{movieId}")
     public Rating getRatings(@PathVariable("movieId") int id){
         return new Rating(id, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRatings getAllRatedMovies(@PathVariable("userId") int userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating(111, 4),
+                new Rating(222, 5)
+        );
+        UserRatings userRatings=new UserRatings();
+        userRatings.setUserRatings(ratings);
+
+        return userRatings;
     }
 }
